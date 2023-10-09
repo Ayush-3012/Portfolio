d893490d34a8849eda8a706edb5d3c077da21e94
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-10-09 15:58:21 +0530

    rectify media queries, and other styling

--- index.js
+++ index.js
@@ -15,59 +15,83 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  fs.readFile(__dirname + "/public/structures/home.html", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading home.html:", err);
-      return res.status(500).send("Error reading home.html");
+  fs.readFile(
+    __dirname + "/public/structures/home.html",
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error("Error reading home.html:", err);
+        return res.status(500).send("Error reading home.html");
+      }
+
+      res.render("index", {
+        sectionTitle: "Home",
+        dataContent: data,
+      });
     }
-    res.render("index", { sectionTitle: "Home", dataContent: data });
-  });
+  );
 });
 
 app.get("/about", async (req, res) => {
-  fs.readFile(__dirname + "/public/structures/about.html", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading about.html:", err);
-      return res.status(500).send("Error reading about.html");
+  fs.readFile(
+    __dirname + "/public/structures/about.html",
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error("Error reading about.html:", err);
+        return res.status(500).send("Error reading about.html");
+      }
+      res.render("index", { sectionTitle: "About Me", dataContent: data });
     }
-    res.render("index", { sectionTitle: "About Me", dataContent: data });
-  });
+  );
 });
 
 app.get("/portfolio", async (req, res) => {
-  fs.readFile(__dirname + "/public/structures/portfolio.html", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading portfolio.html:", err);
-      return res.status(500).send("Error reading portfolio.html");
+  fs.readFile(
+    __dirname + "/public/structures/portfolio.html",
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error("Error reading portfolio.html:", err);
+        return res.status(500).send("Error reading portfolio.html");
+      }
+      res.render("index", { sectionTitle: "My Portfolio", dataContent: data });
     }
-    res.render("index", { sectionTitle: "My Portfolio", dataContent: data });
-  });
+  );
 });
 
 app.get("/trainings", async (req, res) => {
-  fs.readFile(__dirname + "/public/structures/certificates.html", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading certificates.html:", err);
-      return res.status(500).send("Error reading certificates.html");
+  fs.readFile(
+    __dirname + "/public/structures/certificates.html",
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error("Error reading certificates.html:", err);
+        return res.status(500).send("Error reading certificates.html");
+      }
+      res.render("index", {
+        sectionTitle: "My Certifications",
+        dataContent: data,
+      });
     }
-    res.render("index", {
-      sectionTitle: "My Certifications",
-      dataContent: data,
-    });
-  });
+  );
 });
 
 app.get("/contact", async (req, res) => {
-  fs.readFile(__dirname + "/public/structures/contact.html", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading contact.html:", err);
-      return res.status(500).send("Error reading contact.html");
+  fs.readFile(
+    __dirname + "/public/structures/contact.html",
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error("Error reading contact.html:", err);
+        return res.status(500).send("Error reading contact.html");
+      }
+      res.render("index", {
+        sectionTitle: "Connect",
+        dataContent: data,
+      });
     }
-    res.render("index", {
-      sectionTitle: "Connect",
-      dataContent: data,
-    });
-  });
+  );
 });
 
 app.listen(port, () => {
